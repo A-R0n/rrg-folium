7fcd4684cdc245d613172968e32731c2383b5206
Aaron Estes <aestesc@gmail.com>
2019-03-02 18:04:38 -0600

    imported pandas for database

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# rrg-folium
+## Summary
+
+-- Using the folium module with python to pin some of my favorite routes at the Red River Gorge onto a map.
+
+## Interesting Notes
+
+-- The GPS coordinates provided by Mountain Project's API are neither precise or accurate
--- map.py
+++ map.py
@@ -1,15 +1,30 @@
 import folium
-import requests as req
+import pandas as pd
 
-# Create map object
-m = folium.Map(location=[37.8139, -83.6279], zoom_start=12)
+data = pd.DataFrame({
+
+# North and South...greater values are above center
+'lat':[37.6527, 37.65177, 37.7337, 37.8145, 37.73090, 37.7315, 37.7519, 37.75194, 37.6466, 37.6787, 37.6789, 37.6539, 37.6527, 37.72048, 37.72045, 37.7314, 37.7315],
+
+# East and West...greater absolute values are left of center
+'lon':[-83.72515, -83.7098, -83.6388, -83.6639, -83.65582, -83.6565, -83.6615, -83.66370, -83.7229, -83.7356, -83.7358, -83.7176, -83.7177, -83.6628, -83.6627, -83.634, -83.6339], 
+'name_of_route':['Check Your Grip', 'Amarillo Sunset', 'Banshee', 'Mercy the Huff', 'The Return of Chris Snyder', 'Ro Shampo', 'Yellow Brick Road', 'No Place Like Home', "Don't Call It a Comeback", 'Crown of Thorns', 'Dog Bites & Fist Fights', '27 Years of Climbing', 'Starry Night', 'Bare Metal Teen', 'Steelworker', 'Jesus Wept', 'Triple Sec'],
+'grade':['5.12a', '5.11b', '5.11c', '5.12b', '5.11d', '5.12a', '5.11b', '5.11b', '5.11d', '5.11c', '5.12d', '5.8', '5.12a', '5.12a', '5.12c', '5.12d', '5.12d']
+})
+data
+
+
+# Create map object with longitude as the first argument and latitude as the second argument
+m = folium.Map(location=[37.75, -83.7000], zoom_start=11)
 
 # Global Tooltip
 tooltip = 'Click For Route Info'
 # Create markers
-folium.Marker([37.6524, -83.7246],
-    popup='<strong>Breakfast Burrito</strong>',
-    tooltip=tooltip).add_to(m)
+
+# It goes (y axis N<-->S, x axis E<-->W)
+
+for i in range(0,len(data)):
+    folium.Marker([data.iloc[i]['lat'], data.iloc[i]['lon']], popup=folium.Popup(data.iloc[i]['name_of_route'] + '\n' + data.iloc[i]['grade'], max_width=150), tooltip=tooltip, icon=folium.Icon(icon='picture', icon_color='white', color='black')).add_to(m)
 
 # Generate map
 m.save('map.html')
