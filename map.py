0c05c78e8936447e305f5acdb57d9598b8f8ce8a
Aaron Estes <aestesc@gmail.com>
2019-03-03 15:24:12 -0600

    miller fork

--- map.py
+++ map.py
@@ -7,12 +7,12 @@ import numpy as np
 data = pd.DataFrame({
 
 # North and South...greater values are above center
-'lat':[37.6527, 37.65177, 37.7337, 37.8175, 37.73090, 37.7315, 37.7519, 37.75194, 37.6466, 37.6787, 37.6789, 37.6539, 37.6527, 37.72048, 37.72045, 37.7314, 37.7315, 37.7329, 37.6524, 37.8159, 37.80285, 37.8029, 37.8487, 37.8399],
+'lat':[37.6527, 37.65177, 37.7337, 37.8175, 37.73090, 37.7315, 37.7519, 37.75194, 37.6466, 37.6787, 37.6789, 37.6539, 37.6527, 37.72048, 37.72045, 37.7314, 37.7315, 37.7329, 37.6524, 37.8159, 37.80285, 37.8029, 37.8487, 37.8399, 37.6440, 37.6418, 37.6429, 37.6443, 37.6458, 37.6438, 37.6426, 37.6412, 37.6465],
 
 # East and West...greater absolute values are left of center
-'lon':[-83.72515, -83.7098, -83.6388, -83.6639, -83.65582, -83.6565, -83.6615, -83.66370, -83.7229, -83.7356, -83.7358, -83.7176, -83.7177, -83.6628, -83.6627, -83.634, -83.6339, -83.6616, -83.7232, -83.58, -83.5741, -83.5744, -83.6635, -83.6460], 
-'name_of_route':['Check Your Grip', 'Amarillo Sunset', 'Banshee', 'Mercy the Huff', 'The Return of Chris Snyder', 'Ro Shampo', 'Yellow Brick Road', 'No Place Like Home', "Don't Call It a Comeback", 'Crown of Thorns', 'Dog Bites & Fist Fights', '27 Years of Climbing', 'Starry Night', 'Bare Metal Teen', 'Steelworker', 'Jesus Wept', 'Triple Sec', 'Hippocrite', 'Dogleg', 'King Me', 'Prime Directive', 'Orange Juice', 'The Gift', 'Twinkie'],
-'grade':['5.12a', '5.11b', '5.11c', '5.12b', '5.11d', '5.12a', '5.11b', '5.11b', '5.11d', '5.11c', '5.12d', '5.8', '5.12a', '5.12a', '5.12c', '5.12d', '5.12d', '5.12a', '5.12a', '5.11b', '5.11b', '5.12c', '5.12a', '5.12a']
+'lon':[-83.72515, -83.7098, -83.6388, -83.6639, -83.65582, -83.6565, -83.6615, -83.66370, -83.7229, -83.7356, -83.7358, -83.7176, -83.7177, -83.6628, -83.6627, -83.634, -83.6339, -83.6616, -83.7232, -83.58, -83.5741, -83.5744, -83.6635, -83.6460, -83.6785, -83.6840, -83.6820, -83.6795, -83.67999, -83.6795, -83.6855, -83.6805, -83.6802], 
+'name_of_route':['Check Your Grip', 'Amarillo Sunset', 'Banshee', 'Mercy the Huff', 'The Return of Chris Snyder', 'Ro Shampo', 'Yellow Brick Road', 'No Place Like Home', "Don't Call It a Comeback", 'Crown of Thorns', 'Dog Bites & Fist Fights', '27 Years of Climbing', 'Starry Night', 'Bare Metal Teen', 'Steelworker', 'Jesus Wept', 'Triple Sec', 'Hippocrite', 'Dogleg', 'King Me', 'Prime Directive', 'Orange Juice', 'The Gift', 'Twinkie', 'Witness The Citrus', 'Brachial Plexus', 'Fatal Vision', 'Western Blot', 'Grim Reaper', 'Breakfast at Koops', 'Cloudy with a Chance of Seaman', 'Return of the Mohan', 'Dead on Arrival'],
+'grade':['5.12a', '5.11b', '5.11c', '5.12b', '5.11d', '5.12a', '5.11b', '5.11b', '5.11d', '5.11c', '5.12d', '5.8', '5.12a', '5.12a', '5.12c', '5.12d', '5.12d', '5.12a', '5.12a', '5.11b', '5.11b', '5.12c', '5.12a', '5.12a', '5.11c', '5.11c', '5.11c', '5.11d', '5.12a', '5.11d', '5.11d', '5.11d', '5.11d']
 })
 data
 
